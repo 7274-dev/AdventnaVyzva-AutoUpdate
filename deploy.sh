0f9634eb555086e3e35d1e431904a575664ca63d
ivanhrabcak <39710116+ivanhrabcak@users.noreply.github.com>
2020-12-18 09:54:13 +0100

    Updates repository url

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/usr/bash
-echo "it works!"
+sudo systemctl stop apache2
+sudo rm -rf var/www/html/*
+cd ~
+sudo git clone https://github.com/7274-dev/AdventnaVyzva-Stranka.git
+cd ~/AdventnaVyzva-Stranka/website/
+sudo mv ~/AdventnaVyzva-Stranka/website/* /var/www/html
+cd ~
+sudo rm -rf ~/AdventnaVyzva-Stranka
+sudo systemctl start apache2
